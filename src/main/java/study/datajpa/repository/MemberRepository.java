31fcb4338c13b8e9d78b97064138970fae8af0ea
Jhyun-ki <hjk2334@naver.com>
2023-01-10 23:45:36 +0900

    spring data jpa namedQuery

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,22 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findTop3HelloBy();
+
+    //@Query(name = "Member.findByUsername")
+    // 1. 없어도 Member.메서드이름으로 찾는다.
+    // 2. namedQuery없으면 메서드이름으로 쿼리날림.
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
